cc688d56d939a4d49b489fcbec54a389cb9b0e50
aureliengregoire94 <60916671+tributZ@users.noreply.github.com>
2021-04-09 11:46:56 +0200

    ajout du calcul de distance à vol d'oiseau

---
+++ vol_doiseau.c
@@ -0,0 +1,29 @@
+//
+// Created by aurel on 08/04/2021.
+//
+
+#include "vol_doiseau.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+float distance(float latitude1, float longitude1, float latitude2, float longitude2){
+    latitude1*=(M_PI/180);
+    latitude2*=(M_PI/180);
+    longitude2*=(M_PI/180);
+    longitude1*=(M_PI/180);
+    float a = sinf((latitude1-latitude2)/2);
+    a *=a;
+    printf("%f \n", a);
+    float b = sinf((longitude1-longitude2)/2);
+    b*=b;
+    printf("%f \n", b);
+    b*= cosf(latitude2)*cosf(latitude1);
+    a+=b;
+    float c = 2*atanf((sqrtf(a)/sqrtf(1-a)));
+    c*=6731;
+    printf("%f \n", c);
+    return c;
+}
+
---
+++ vol_doiseau.h
@@ -0,0 +1,10 @@
+//
+// Created by aurel on 08/04/2021.
+//
+
+#ifndef ITINERROPTI_VOL_DOISEAU_H
+#define ITINERROPTI_VOL_DOISEAU_H
+
+float distance(float latitude1, float longitude1, float latitude2, float longitude2);
+
+#endif //ITINERROPTI_VOL_DOISEAU_H
