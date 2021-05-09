f91797aeb35adc4694fa416d9a1d4cef9f832002
aureliengregoire94 <60916671+tributZ@users.noreply.github.com>
2021-05-09 16:58:32 +0200

    ekip ekip

--- path_time_based.py
+++ path_time_based.py
@@ -1,6 +1,8 @@
 import json
 import os
+import string
 
+transports = ['walking','bicycling','transit','driving']
 def get_time(point,adress,transport_method):
     for destination in point:
         if destination['destination'] == adress:
@@ -8,23 +10,41 @@ def get_time(point,adress,transport_method):
                 if transport['transport'] == transport_method:
                     return transport['duration']['value']
 
-
+with open('Set/Set.json') as file:
+    data = json.load(file)
 
 keys = [key for key in data.keys()]
 
 adress = "13 all\u00c3\u00a9e Berlioz 94800 Villejuif France"
+adress_list = []
 
 for key in data.keys():
     if key == adress:
         point = data[key]
-
-print(point)
-
-def main(adress):
-    with open('Set/Set.json') as file:
-        data = json.load(file)
+    adress_list.append(key)
 
 
-transports = ['walking','bicycling','transit','driving']
-if __name__ == '__main__':
-    main("13 all\u00c3\u00a9e Berlioz 94800 Villejuif France")
+
+def pathTime(list, data, transport_method):
+    list2 = [list[0]]
+    list.pop(0)
+    for i in range(len(list)):
+        pos = list2[-1]
+        min =  list[0]
+        time = get_time(data[pos], min, transport_method)
+        for point in list[1:]:
+            time2 = get_time(data[pos], point, transport_method)
+            if time>time2:
+                min = point
+                time = time2
+        list2.append(min)
+        list.remove(min)
+    return list2
+
+print(pathTime(adress_list, data, transports[0]))
+
+
+
+
+
+
